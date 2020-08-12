5e412d4cc7c957e797a7884089a655f2342b339c
simpart <simpart>
2020-08-12 02:57:48 +0000

    init

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # mofron-comp-winona
-winona style button component for mofron
+[mofron](https://mofron.github.io/mofron/) is module based frontend framework.
+
+component module template for developper
+
+
+# Install
+```
+npm install mofron mofron-comp-winona
+```
+
+# Parameter
+
+| Short<br>Form | Parameter Name | Type | Description |
+|:-------------:|:---------------|:-----|:------------|
+| ◯  | text | mixed | string: button text contents |
+| | | | mofron-comp-text: button text component |
+| | | mixed | text component config |
+| | baseColor | mixed (color) | string: button background color name, #hex |
+| | | | array: [red, green, blue, (alpha)] |
+
---
+++ index.js
@@ -0,0 +1,113 @@
+/**
+ * @file mofron-comp-winona/index.js
+ * @brief component module template for developper
+ * @license MIT
+ */
+const Button = require("mofron-comp-button");
+const Style  = require("mofron-event-style");
+const css    = require("./winona.css");
+const comutl = mofron.util.common;
+
+module.exports = class extends Button {
+    /**
+     * initialize component
+     * 
+     * @param (mixed) text: button text contents
+     *                dict: component config
+     * @short text
+     * @type private
+     */
+    constructor (p1) {
+        try {
+            super();
+            this.name("Winona");
+	    if (0 < arguments.length) {
+                this.config(p1);
+            }
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * initialize dom contents
+     * 
+     * @type private
+     */
+    initDomConts () {
+        try {
+            super.initDomConts();
+	    this.childDom().class("winona");
+
+            comutl.addstyle("winona", css, false);
+	    
+            this.text().event(new Style(
+	        (fs1,fs2) => {
+		    try {
+		        comutl.addstyle(
+			    "winona",
+			    ".winona::after{font-size:" + fs2[0]["font-size"] + ";}"
+			);
+		    } catch (e) {
+                        console.error(e.stack);
+                        throw e;
+		    }
+		},
+		"font-size"
+	    ));
+	    
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    /**
+     * button text contents
+     * 
+     * @param (mixed) string: button text contents
+     *                mofron-comp-text: button text component
+     * @param (mixed) text component config
+     * @return (mofron-comp-text) button text
+     * @type parameter
+     */
+    text (prm, cnf) {
+        try {
+	    if (undefined !== prm) {
+                this.childDom().attrs({
+		    "data-text" : (true === comutl.iscmp(prm)) ? prm.text() : prm
+		});
+	    }
+	    return super.text(prm, cnf);
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
+    }
+    
+    /**
+     * button background color
+     *
+     * @param (mixed (color)) string: button background color name, #hex
+     *                        array: [red, green, blue, (alpha)]
+     * @return (mixed) button background color
+     *                 null: not set yet
+     * @type parameter
+     */
+    baseColor (prm) {
+        try {
+	    if (undefined !== prm) {
+	        let style = comutl.obj2style(
+	            "#" + this.childDom().id() + ":hover",
+		    { "background-color" : comutl.getcolor(prm).toString() }
+                );
+                comutl.addstyle(this.id(), style);
+            }
+	} catch (e) {
+            console.error(e.stack);
+            throw e;
+	}
+    }
+}
+/* end of file */
